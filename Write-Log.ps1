fceb2e39715b91ffa94228984b5f23d17c2c668e
ctrl-freak <bryce.sheehan@gmail.com>
2017-08-07 09:31:02 +0800

    Add files via upload

---
+++ Write-Log.ps1
@@ -0,0 +1,12 @@
+﻿Function Write-Log($string, $color) {
+# Colors: [enum]::GetValues([System.ConsoleColor]) | Foreach-Object {Write-Host $_ -ForegroundColor $_}
+    $timestamp = Get-Date -Format "s"
+    $oldFGcolor = $host.ui.RawUI.ForegroundColor
+    if ($color -ne $null) {
+        $host.ui.RawUI.ForegroundColor = $color
+        Write-Output ("["+$timestamp+"] " + $string)
+        $host.ui.RawUI.ForegroundColor = $oldFGcolor
+    } else {
+        Write-Output ("["+$timestamp+"] " + $string)
+    }
+}
