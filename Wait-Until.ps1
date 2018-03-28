dcee5fcd43d776e1283810c81baecfe6046ea46a
ctrl-freak <bryce.sheehan@gmail.com>
2018-03-28 10:08:14 +0800

    Add Wait-Until.ps1

---
+++ Wait-Until.ps1
@@ -0,0 +1,12 @@
+﻿Function Wait-Until {
+    param (
+        [Parameter(Mandatory=$True,Position=1)][string] $Script,
+        [int] $Pause = 1
+    )
+
+    $ScriptBlock = [scriptblock]::Create($Script)
+
+    While ((Invoke-Command -ScriptBlock $ScriptBlock) -eq $False) {
+        Start-Sleep $Pause
+    }
+}
