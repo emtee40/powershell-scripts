791d4aad384d54aa403dfa3541a5fc7bb1f2971c
ctrl-freak <bryce.sheehan@gmail.com>
2018-12-12 12:28:54 +0800

    Add files via upload

---
+++ Get-SetExtensionAttributes.ps1
@@ -0,0 +1,14 @@
+﻿param (
+    [string] $DomainController = (Get-ADDomain | Select-Object -Expand PDCEmulator),
+    [Array] $Attributes = (1..15 | % {"extensionAttribute$_"})
+)
+
+Import-Module ActiveDirectory
+
+ForEach ($Attribute in $Attributes) {
+    $AttributeUsers = Get-ADUser -Properties $Attribute -Filter {$Attribute -like "*"} -Server $DomainController
+
+    If ($AttributeUsers.Count -gt 0) {
+        $AttributeUsers | Select sAMAccountName,Name,$Attribute | FT
+    }
+}
